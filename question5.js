c69f5ac2f004d98178fd88ce7ae5d125680bfb83
unknown <YY128@ccs.local>
2025-03-10 18:10:47 +0800

    New Update

--- question5.js
+++ question5.js
@@ -10,7 +10,7 @@ sequelize.sync()
     .then(() => console.log('All models synced!'))
     .catch((err) => console.error('Sync error:', err));
 
-// Route to fetch all users
+// Route to fetch all users 
 app.get('/users', async (req, res) => {
     try {
         const users = await User.findAll();
