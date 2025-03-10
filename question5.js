e9e16aabf4eb3d64e87d40538361fe7fceecf2df
unknown <YY128@ccs.local>
2025-03-10 18:01:21 +0800

    Add question5

---
+++ question5.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const User = require('./models/User');
+const sequelize = require('./db');
+
+const app = express();
+const PORT = 3000;
+
+// Sync Sequelize models
+sequelize.sync()
+    .then(() => console.log('All models synced!'))
+    .catch((err) => console.error('Sync error:', err));
+
+// Route to fetch all users
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.findAll();
+        res.json(users);
+    } catch (error) {
+        res.status(500).send('Error fetching users');
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
